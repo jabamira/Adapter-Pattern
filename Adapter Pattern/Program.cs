65bca80bcf13bf8607d90c1f2d94c9c5d2b90e48
artem <artem@ZALMAN>
2024-10-17 14:58:40 +0700

    Add project files.

---
+++ Adapter Pattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using Adapter_Pattern;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ComputerGame computerGame = new ComputerGame(name:"Red Dead Redemption",
+            pegiAgeRating:PegiAgeRating.P7,budgetInMillionsOfDollars:65,minimumGpuMemoryInMegabytes:8,diskSpaceNeededInGB:122,ramNeededInGb:16,coresNeeded:8,coreSpeedInGhz:3.1);
+        ComputerGameAdapter adapt = new ComputerGameAdapter(computerGame);
+        Console.Write(adapt.getTitle());
+        if (adapt.isTripleAGame())
+        {
+            Console.WriteLine(" AAA Game");
+        }
+        else
+        {
+            Console.WriteLine(" Indi game");
+        }
+        Console.WriteLine(adapt.getPegiAllowedAge());
+        Console.WriteLine(adapt.getRequirements().ToString());
+       
+    }
+}
